15ffcaad7960f3b0a79e145f9dab80333bb43e02
roshnitandon <50960010+roshnitandon@users.noreply.github.com>
2019-06-03 11:46:18 -0400

    base code for timer
    I dont know why the javascript I did isnt showing up. I honestly don't even know if it's connecting.

---
+++ Timer/stopwatch.js
@@ -0,0 +1,55 @@
+function Stopwatch() {
+    var time = 0;
+    var interval;
+    var offset;
+    function update() {
+        time+=timePassed();
+        var formattedTime = timeFormatter(time);
+        console.log(formattedTime)
+        Element.textContent = formattedTime;
+    }
+    function timePassed() {
+        var now = Date.now();
+        var timePassed = now - offset;
+        offset = now;
+        return timePassed;
+    }
+    function timeForamtter(timeInMilliseconds) {
+        var time = new Date(timeInMilliseconds);
+        var minutes = time.getMinutes().toString();
+        var seconds = time.getSeconds().toString();
+        var milliseconds = time.milliseconds().toString();
+        if(minutes.length <2) {
+            minutes = "0" + minutes;
+        }
+        if(seconds.length <2) {
+            seconds = "0" + seconds;
+        }
+       
+        while(milliseconds.length <3) {
+            milliseconds = "0" + milliseconds;
+        }
+        return minutes + " : " + seconds + " . " + milliseconds;
+    }
+ 
+    this.isOn = false;
+    this.start = function() {
+        if(!this.isOn){
+            interval = setInterval(update, 10);
+            offset = Date.now();
+            this.isOn = true;
+        }
+    };
+    this.stop = function() {
+        if(this.isOn){
+            clearInterval(interval);
+            interval = null;
+            this.isOn = false;
+        }
+    };
+    this.reset = function() {
+        time = 0;
+        update();
+    };
+ }
+ 
