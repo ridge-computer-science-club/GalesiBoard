8f4de3acfa7e27a20cb2287a63039b61ff940db3
Nastassja Kuznetsova <nastassjakuz@gmail.com>
2019-06-16 19:09:47 -0400

    Post request for input

---
+++ dist/js/input.js
@@ -0,0 +1,30 @@
+
+function changeText() {
+    var userIn = document.getElementById("textIn").value;
+
+    document.getElementById("display").innerHTML = userIn;
+}
+
+function postInput() {
+    //Need to change Url to GalesiBoard. It is currently just a website that tests post requests.
+    const Url = 'http://localhost:8080/post/';
+    const dada = {
+        input: document.getElementById("textIn").value
+    }
+
+    test = fetch(Url, {
+        method: 'POST',
+        mode: 'same-origin',
+        headers: {
+            'Accept': 'text/plain',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(dada)
+    }).then((result) => {
+        console.log(result);
+        return result.text();
+    }).then(data => {
+        console.log(data);
+        return data;
+    });
+}
--- dist/js/weather.js
+++ dist/js/weather.js
@@ -74,7 +74,7 @@ function post() {
     }); */
 }
 
-function everything() {
+function postWeatherDateTime() {
     dateTime();
     weather();
 }
