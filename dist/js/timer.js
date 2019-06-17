563656e3720c049fed4e63133b3736df29331e43
Nastassja Kuznetsova <nastassjakuz@gmail.com>
2019-06-17 11:25:28 -0400

    centered & fixed timer

---
+++ dist/js/timer.js
@@ -0,0 +1,19 @@
+var timer = document.getElementById("timer");
+var toggleBtn = document.getElementById("toggle");
+var resetBtn = document.getElementById("reset");
+var watch = new StopWatch(timer);
+
+toggleBtn.addEventListener("click", function() {
+    if(watch.isOn){
+        watch.stop();
+        document.getElementById("toggle").innerHTML = "Start";
+
+    } else {
+        watch.start();
+        document.getElementById("toggle").innerHTML = "Stop";
+    }
+});
+
+resetBtn.addEventListener("click", function () {
+    watch.reset();
+});
